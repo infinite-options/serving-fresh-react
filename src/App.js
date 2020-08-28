d98fff54e13e89d482af0408d0ff0c9a1193996d
Zachary Lind <zlind@Zacharys-MacBook-Pro.local>
2020-08-27 21:45:03 -0700

    removed starter code, added routes and home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path='/'>
+          <Redirect to='/home'/>
+        </Route>
+        <Route exact path='/home' component={Home}/>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
