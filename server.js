a026c0ea05dcdc2472ce98f71429317f23c43df3
Lyman Shen <lyman.shen@gmail.com>
2020-09-30 22:53:40 -0700

    For Google CLoud Platform Deploy

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const https = require('https');
 const fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync('/etc/letsencrypt/live/servingfresh.me/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/servingfresh.me/cert.pem')
 }
 
 const app = express();
